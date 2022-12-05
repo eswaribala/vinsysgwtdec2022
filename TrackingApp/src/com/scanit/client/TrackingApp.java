a4463f97460f4471f0e0e7e451da18510c36eb77
eswaribala <parameswaribala@gmail.com>
2022-12-05 15:45:25 +0530

    updated

--- TrackingApp/src/com/scanit/client/TrackingApp.java
+++ TrackingApp/src/com/scanit/client/TrackingApp.java
@@ -2,7 +2,7 @@ package com.scanit.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 
@@ -15,14 +15,15 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.scanit.server.LoginServiceImpl;
 
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class TrackingApp implements EntryPoint {
+public class TrackingApp implements EntryPoint,ClickHandler {
 	
-	
+	private LoginServiceAsync loginServiceAsync=GWT.create(LoginServiceImpl.class);
 	
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
@@ -31,7 +32,9 @@ public class TrackingApp implements EntryPoint {
 
 		// We can add style names to widgets
 		sendButton.addStyleName("sendButton");
+		sendButton.addClickHandler(this);
 		
+		/* client side call
 		sendButton.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -42,12 +45,18 @@ public class TrackingApp implements EntryPoint {
 			}
 			
 		});
-
+       */
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
 		RootPanel.get("nameFieldContainer").add(nameField);
 		RootPanel.get("sendButtonContainer").add(sendButton);
 	
+		
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {
+		// TODO Auto-generated method stub
 		
 	}
 }
