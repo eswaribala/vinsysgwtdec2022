2df31610f467f3189d56ba47bf463f1587bc3b7b
eswaribala <parameswaribala@gmail.com>
2022-12-05 15:27:20 +0530

    updated

---
+++ TrackingApp/src/com/scanit/server/LoginServiceImpl.java
@@ -0,0 +1,13 @@
+package com.scanit.server;
+
+import com.scanit.client.LoginService;
+
+public class LoginServiceImpl implements LoginService{
+
+	@Override
+	public String showMessage(String name) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
